cc4e5aa475843a5bc4e1469418cbfb85f57d7dab
zclkkk <2484392714@qq.com>
2021-07-14 22:42:28 +0800

    11111

---
+++ build.sh
@@ -0,0 +1,10 @@
+CLANG_DIR=../clang
+args="O=../out \
+	ARCH=arm64 \
+	CC=${CLANG_DIR}/bin/clang \
+	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+	LD=${CLANG_DIR}/bin/ld.lld "
+make ${args} umi_defconfig
+make -j$(nproc --all) ${args}
